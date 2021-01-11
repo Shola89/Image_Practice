2f532fe99bf8c47f73053dd5293723d4cb5b288c
Shola89 <mishaalgaming1@gmail.com>
2021-01-11 17:09:11 +0300

    Fixed the save line to use correct object

---
+++ main.py
@@ -0,0 +1,20 @@
+#Script to resize, change format & rotate images
+import os
+from PIL import Image
+
+#Resizes image and rotates it
+def geometry(pic):
+    im = pic.resize((128, 128))
+    im = pic.rotate(270)
+    return im
+
+def main():
+    for i in os.listdir("."):
+        if i.endswith('.jpg'):
+            pic = Image.open(i)
+            fname, filext = os.path.splitext(i)
+            pic = geometry(pic)
+            pic.save('converted/{}.png'.format(fname))
+
+main()
+
